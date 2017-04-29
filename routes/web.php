f7b9271fb5ccbcb72464584405b40d678973c5dd
Murtaza Hussain <MHussain@bluechipww.com>
2017-04-29 15:25:49 -0500

    set up routes

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,18 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/index', function () {
+    return view('index');
+});
+
+Route::get('/bills', 'BillsController@getAllBills');
+Route::get('/bills/{bill_id}', 'BillsController@getBill');
+
+Route::get('/members/{members}', 'MembersController@getAllMembers');
+Route::get('/members/{member_id}', 'MembersController@getMember');
+
+Route::get('/committees', 'CommitteeController@getAllCommittees');
+Route::get('/committee/{committee_id}', 'CommitteeController@getCommittee');
+
+Route::get('/search', 'SearchController@getAll');
